7a8df88645b24d888af98bf3c9c95600486cd98a
raviwbc <sraviwbc@gmail.com>
2025-03-22 12:15:09 +0530

    interceptor

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ import { store } from './store/store.ts';
 import { CompletedList } from './component/completedTaskList/completedList.tsx';
 import Tie from './pages/games/tie.tsx';
 import { Login } from './component/login/login.tsx';
+import { BrowserRouter, Link, Route, Router, Routes } from 'react-router-dom';
 
 function App() {
   const [theme, setTheme] = useState('dark');
@@ -30,18 +31,36 @@ function App() {
 
   return (
     <div>
+          <BrowserRouter>
+          {/* <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+        </ul>
+      </nav> */}
       <Provider store={store}>
       <MyContext.Provider value={contextValue}>
-        <div className={theme}>
-        <HeaderComp></HeaderComp>
-        <TimeTrack />
-        <Login/>
-        {/* <Tie/> */}
+      <div className={theme}>
+      <HeaderComp></HeaderComp>
+      {/* <Router> */}
+      <Routes>
+        <Route path="/" element={<TimeTrack />}/>
+        <Route path="/login" element={<Login />}/>
+        <Route path="*" element={<TimeTrack />} />
+
+      </Routes>
+      {/* </Router> */}
+        
+        
+        
+        
+        
         
         </div>
         </MyContext.Provider>
         </Provider>
-      
+        </BrowserRouter>
     </div>
   );
 }
