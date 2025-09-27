b44db39b5750bb995803e7725580ebe4bc79f066
sanjaisanthosh <sanjaiwbcsoftwarelab@gmail.com>
2025-09-27 17:05:46 +0530

    Loader delay

--- src/component/completedTaskList/loader/loader.tsx
+++ src/component/completedTaskList/loader/loader.tsx
@@ -1,20 +1,35 @@
 // src/components/ClockLoader.tsx
-import React, { useEffect } from 'react';
-import './loader.css';
-import { useSelector } from 'react-redux';
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import "./loader.css";
 
-const ClockLoader = () => {
-  const loading = useSelector((state: any) => state.loaderStatus.loading); 
-  if (!loading) return null;
-    return (
-      <div className="clock-loader">
-        <div className="bg_black_div"></div>
-        <div className="clock-face">
-          <div className="clock-hand"></div>
-          <div className="clock-small-hand"></div>
-        </div>
+const ClockLoader: React.FC = () => {
+  const loading = useSelector((state: any) => state.loaderStatus.loading);
+  const [showLoader, setShowLoader] = useState(false);
+
+  useEffect(() => {
+    let timer: NodeJS.Timeout;
+
+    if (loading) {
+      setShowLoader(true);
+    } else {
+      timer = setTimeout(() => setShowLoader(false), 300);
+    }
+
+    return () => clearTimeout(timer);
+  }, [loading]);
+
+  if (!showLoader) return null;
+
+  return (
+    <div className="clock-loader">
+      <div className="bg_black_div"></div>
+      <div className="clock-face">
+        <div className="clock-hand"></div>
+        <div className="clock-small-hand"></div>
       </div>
-    );
-  };
+    </div>
+  );
+};
 
 export default ClockLoader;
